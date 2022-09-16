43932491c5365cc67c19287481914b939c619359
Attia Ahmed <attia.a.ahmed@gmail.com>
2022-09-17 00:42:35 +0300

    dynamic extra headers

--- lib/src/network/api.dart
+++ lib/src/network/api.dart
@@ -5,6 +5,12 @@ import 'package:one_studio_core/src/injection/injection_container.dart';
 import 'package:package_info_plus/package_info_plus.dart';
 
 class Api {
+  static Map<String, String> Function()? extraHeaders;
+
+  void setExtraHeaders(Map<String, String> Function() extraHeaders) {
+    Api.extraHeaders = extraHeaders;
+  }
+
   static Map<String, String> headers({Map<String, String>? extra}) {
     Map<String, String> map = <String, String>{};
     map['content-type'] = 'application/json';
@@ -16,7 +22,9 @@ class Api {
       map['app-versionName'] = sl<PackageInfo>().version;
       map['app-version'] = sl<PackageInfo>().buildNumber;
     }
-
+    if (extraHeaders != null) {
+      map.addAll(extraHeaders!());
+    }
     if (extra != null) {
       map.addAll(extra);
     }
