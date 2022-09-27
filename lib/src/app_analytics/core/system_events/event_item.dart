e518331cde03bfad5d376c3898e10ec43ea9c68c
Salah Nahed <salahamassi@users.noreply.github.com>
2022-09-28 01:47:03 +0400

    add from map and to map

--- lib/src/app_analytics/core/system_events/event_item.dart
+++ lib/src/app_analytics/core/system_events/event_item.dart
@@ -48,4 +48,60 @@ class EventItem {
     this.promotionName,
     this.quantity,
   });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'affiliation': affiliation,
+      'currency': currency,
+      'coupon': coupon,
+      'creativeName': creativeName,
+      'creativeSlot': creativeSlot,
+      'discount': discount,
+      'index': index,
+      'itemBrand': itemBrand,
+      'itemCategory': itemCategory,
+      'itemCategory2': itemCategory2,
+      'itemCategory3': itemCategory3,
+      'itemCategory4': itemCategory4,
+      'itemCategory5': itemCategory5,
+      'itemId': itemId,
+      'itemListId': itemListId,
+      'itemListName': itemListName,
+      'itemName': itemName,
+      'itemVariant': itemVariant,
+      'locationId': locationId,
+      'price': price,
+      'promotionId': promotionId,
+      'promotionName': promotionName,
+      'quantity': quantity,
+    };
+  }
+
+  factory EventItem.fromMap(Map<String, dynamic> map) {
+    return EventItem(
+      affiliation: map['affiliation'] as String,
+      currency: map['currency'] as String,
+      coupon: map['coupon'] as String,
+      creativeName: map['creativeName'] as String,
+      creativeSlot: map['creativeSlot'] as String,
+      discount: map['discount'] as num,
+      index: map['index'] as int,
+      itemBrand: map['itemBrand'] as String,
+      itemCategory: map['itemCategory'] as String,
+      itemCategory2: map['itemCategory2'] as String,
+      itemCategory3: map['itemCategory3'] as String,
+      itemCategory4: map['itemCategory4'] as String,
+      itemCategory5: map['itemCategory5'] as String,
+      itemId: map['itemId'] as String,
+      itemListId: map['itemListId'] as String,
+      itemListName: map['itemListName'] as String,
+      itemName: map['itemName'] as String,
+      itemVariant: map['itemVariant'] as String,
+      locationId: map['locationId'] as String,
+      price: map['price'] as num,
+      promotionId: map['promotionId'] as String,
+      promotionName: map['promotionName'] as String,
+      quantity: map['quantity'] as int,
+    );
+  }
 }
