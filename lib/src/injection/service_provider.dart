f6a661cab9fd79775a4e5beb7ef6aa2d78ce59fa
Salah Nahed <salahamassi@users.noreply.github.com>
2022-10-02 22:18:14 +0400

    Update service_provider.dart

--- lib/src/injection/service_provider.dart
+++ lib/src/injection/service_provider.dart
@@ -1,11 +1,11 @@
 import 'package:get_it/get_it.dart';
 
-import '../notifications.dart' show NotificationProvider;
-
 abstract class ServiceProvider {
   Future<void> register(GetIt it);
 
   T? responseConvert<T>(Map<String, dynamic> json) => null;
 
-  void registerNotifications(List<NotificationProvider> providers) {}
+  void registerNotifications() {}
+
+  void unregisterNotifications() {}
 }
