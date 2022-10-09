c15729194581bb573b86899bf7708ebeabc63c61
Salah Nahed <salahamassi@users.noreply.github.com>
2022-10-09 07:46:59 +0300

    Update notification_provider.dart

--- lib/src/notifications/core/notification_provider.dart
+++ lib/src/notifications/core/notification_provider.dart
@@ -7,39 +7,3 @@ abstract class NotificationProvider {
 
   Future<void> dispose();
 }
-
-//
-//
-// class GoogleCloudMessagingProvider with PushNotificationProvider {
-//   @override
-//   // TODO: implement apnsToken
-//   Future<String?> get apnsToken => throw UnimplementedError();
-//
-//   @override
-//   Future<void> deleteToken() {
-//     // TODO: implement deleteToken
-//     throw UnimplementedError();
-//   }
-//
-//   @override
-//   // TODO: implement initialNotification
-//   Future<NotificationData?> get initialNotification =>
-//       throw UnimplementedError();
-//
-//   @override
-//   // TODO: implement onPushNotification
-//   Stream<NotificationData> get onPushNotification => throw UnimplementedError();
-//
-//   @override
-//   // TODO: implement onPushNotificationTapped
-//   Stream<NotificationData> get onPushNotificationTapped =>
-//       throw UnimplementedError();
-//
-//   @override
-//   // TODO: implement onTokenRefresh
-//   Stream<String> get onTokenRefresh => throw UnimplementedError();
-//
-//   @override
-//   // TODO: implement token
-//   Future<String?> get token => throw UnimplementedError();
-// }
