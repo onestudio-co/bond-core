827c66af3e833433e30c699d8b12759df7c53db2
Salah Nahed <salahamassi@users.noreply.github.com>
2022-11-15 20:49:01 +0200

    NotificationCenterDataSource extends DataSource

--- lib/src/notifications/server_notifications/notification_center/notification_center_data_source.dart
+++ lib/src/notifications/server_notifications/notification_center/notification_center_data_source.dart
@@ -1,6 +1,6 @@
 import 'package:one_studio_core/core.dart';
 
-abstract class NotificationCenterDataSource {
+abstract class NotificationCenterDataSource extends DataSource {
   Future<ListResponse<ServerNotificationModel>> loadNotifications();
 
   Future<void> read(String uuid);
