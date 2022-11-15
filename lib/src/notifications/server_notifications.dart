60139b78ce5b0cf86d9eaf37bca4142fe1128b6b
Salah Nahed <salahamassi@users.noreply.github.com>
2022-11-15 21:21:49 +0200

    add missing export

--- lib/src/notifications/server_notifications.dart
+++ lib/src/notifications/server_notifications.dart
@@ -1,5 +1,6 @@
 library server_notifcations;
 
 export 'server_notifications/notification_center.dart';
+export 'server_notifications/server_notification_data.dart';
 export 'server_notifications/server_notification_model.dart';
 export 'server_notifications/server_notifications_provider.dart';
