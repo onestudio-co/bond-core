2c29153df6303b5021324d30aa2cb5fb29cefd72
Salah Nahed <17902030+salahamassi@users.noreply.github.com>
2023-05-22 00:08:45 +0300

    SuccessResponse jsonable now

--- lib/src/network/models/success_response.dart
+++ lib/src/network/models/success_response.dart
@@ -1,12 +1,13 @@
 import 'package:equatable/equatable.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+import 'jsonable.dart';
 import 'meta.dart';
 
 part 'success_response.g.dart';
 
 @JsonSerializable(explicitToJson: true)
-class SuccessResponse extends Equatable {
+class SuccessResponse extends Equatable with Jsonable {
   const SuccessResponse({required this.message, this.meta});
 
   final String message;
@@ -15,6 +16,7 @@ class SuccessResponse extends Equatable {
   factory SuccessResponse.fromJson(Map<String, dynamic> json) =>
       _$SuccessResponseFromJson(json);
 
+  @override
   Map<String, dynamic> toJson() => _$SuccessResponseToJson(this);
 
   @override
