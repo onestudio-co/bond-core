27361157d7e9ae5d316186536d8653c4065a0575
Salah Nahed <17902030+salahamassi@users.noreply.github.com>
2023-06-15 01:18:12 +0300

    wip

---
+++ lib/src/form/form_fields/radio_group_field_state.dart
@@ -0,0 +1,18 @@
+import 'package:bond_core/src/form/validation/validation_rule.dart';
+
+import 'form_field_state.dart';
+import 'radio_button_field_state.dart';
+
+class RadioGroupFieldState<T> extends FormFieldState<T> {
+  final List<RadioButtonFieldState<T>> radioButtons;
+
+  RadioGroupFieldState(
+    this.radioButtons, {
+    required String label,
+    T? value,
+    List<ValidationRule<T>> rules = const [],
+  }) : super(
+            value: value ?? radioButtons.first.value,
+            label: label,
+            rules: rules);
+}
