59d63c6edeb49c772dda2bde442ed4362eb7e4f4
Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>
2023-06-19 09:03:55 +0300

    add chatBotId

--- lib/src/chat_bot/models/chat_message.dart
+++ lib/src/chat_bot/models/chat_message.dart
@@ -7,12 +7,14 @@ part 'message_type.dart';
 part 'chat_message_convertible.dart';
 
 class ChatMessage {
+  final int chatBotId;
   final MessageSender sender;
   final MessageType type;
   final String content;
   final List<String>? choices; // only used if type is MessageType.multiChoice
 
   ChatMessage({
+    required this.chatBotId,
     required this.sender,
     required this.type,
     required this.content,
