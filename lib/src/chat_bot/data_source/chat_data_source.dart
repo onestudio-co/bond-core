1a5240f0e8010b8bd7f50ed3199d26a4b414e941
Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>
2023-06-19 16:02:19 +0300

    refactor chatBotId in sendMessage in ChatController

--- lib/src/chat_bot/data_source/chat_data_source.dart
+++ lib/src/chat_bot/data_source/chat_data_source.dart
@@ -5,8 +5,5 @@ import '../models/chat_message.dart';
 abstract class ChatDataSource<T extends ChatMessageConvertible> {
   Future<ListResponse<T>> loadMessages(int chatBotId);
 
-  Future<ListResponse<T>> sendMessage({
-    Map<String, dynamic> body,
-    int chatBotId,
-  });
+  Future<ListResponse<T>> sendMessage(Map<String, dynamic> body, int chatBotId);
 }
