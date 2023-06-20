37a4405a51ffa35df955e528db441ef502c94b2b
Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>
2023-06-20 13:08:01 +0300

    Add ListMResponse

---
+++ lib/src/network/models/list_m_response.dart
@@ -0,0 +1,36 @@
+import 'package:bond_core/core.dart';
+import 'package:bond_core/src/network/models/response_converter.dart';
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'list_m_response.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class ListMResponse<T extends Model, G extends Jsonable> extends Equatable
+    with Jsonable {
+  @ResponseConverter()
+  final List<T> data;
+  @ResponseConverter()
+  final G meta;
+
+  const ListMResponse(this.data, this.meta);
+
+  @override
+  Map<String, dynamic> toJson() => _$ListMResponseToJson(this);
+
+  factory ListMResponse.fromJson(Map<String, dynamic> json) =>
+      _$ListMResponseFromJson(json);
+
+  ListMResponse<T, G> copyWith({
+    List<T>? data,
+    G? meta,
+  }) {
+    return ListMResponse<T, G>(
+      data ?? this.data,
+      meta ?? this.meta,
+    );
+  }
+
+  @override
+  List<Object?> get props => [data, meta];
+}
