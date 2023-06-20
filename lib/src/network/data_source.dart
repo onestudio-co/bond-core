32840ea50cdf6374d5dd8a33be2f8bdae52539d0
Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>
2023-06-20 14:00:32 +0300

    refactor list_m_response and data source

--- lib/src/network/data_source.dart
+++ lib/src/network/data_source.dart
@@ -34,6 +34,14 @@ class DataSource {
           json.encode(response.data), response.statusCode);
     }
   }
+  ListMResponse<T,G> mapListMResponse<T extends Model,G extends Jsonable>(Response<dynamic> response) {
+    if ((response.statusCode ?? 0) <= 204) {
+      return ListMResponse<T,G>.fromJson(response.data);
+    } else {
+      throw ServerException.fromResponse(
+          json.encode(response.data), response.statusCode);
+    }
+  }
 
   SuccessResponse mapSuccessResponse(Response response) {
     if ((response.statusCode ?? 0) <= 204) {
--- lib/src/network/models.dart
+++ lib/src/network/models.dart
@@ -3,6 +3,7 @@ library models;
 export 'models/jsonable.dart';
 export 'models/links.dart';
 export 'models/list_response.dart';
+export 'models/list_m_response.dart';
 export 'models/meta.dart';
 export 'models/model.dart';
 export 'models/single_m_response.dart';
