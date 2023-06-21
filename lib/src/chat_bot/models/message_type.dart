4bcbf79193bfe038ac3b348bebd00cdae8238abc
Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>
2023-06-21 22:24:53 +0300

    add ad in MessageType to show ad widget

--- lib/src/chat_bot/models/message_type.dart
+++ lib/src/chat_bot/models/message_type.dart
@@ -5,4 +5,5 @@ enum MessageType {
   question, // a question that requires a textual answer
   multiChoice, // a question that requires selecting an answer from multiple choices
   stepper, // a question that requires selecting a value from a range of values
+  ad, // a plain text message with button to show ad
 }
