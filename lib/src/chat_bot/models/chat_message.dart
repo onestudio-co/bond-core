b118a30aa334e9ad20f7d9634315557c0da44483
Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>
2023-06-21 16:02:37 +0300

    refactor naming message icon in chat message

--- lib/src/chat_bot/models/chat_message.dart
+++ lib/src/chat_bot/models/chat_message.dart
@@ -16,7 +16,7 @@ class ChatMessage {
   final MessageSender sender;
   final MessageType type;
   final String content;
-  final String? messageIcon;
+  final String? icon;
   final String? key; //only used if type is MessageType.multiChoice key for multi-choice selection
   final List<Choice>? choices; // only used if type is MessageType.multiChoice
 
@@ -26,7 +26,7 @@ class ChatMessage {
     required this.sender,
     required this.type,
     required this.content,
-    this.messageIcon,
+    this.icon,
     this.key,
     this.choices,
   });
