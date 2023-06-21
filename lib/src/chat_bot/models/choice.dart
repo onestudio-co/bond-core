257148a6baea7d537981f00b86bd1d58e8870609
Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>
2023-06-21 12:58:06 +0300

    add key for choices

--- lib/src/chat_bot/models/choice.dart
+++ lib/src/chat_bot/models/choice.dart
@@ -3,6 +3,7 @@ part of 'chat_message.dart';
 class Choice {
   String value;
   String text;
+  String key;
 
-  Choice({required this.value, required this.text});
+  Choice({required this.value, required this.text,required this.key});
 }
