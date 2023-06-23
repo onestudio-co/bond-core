738e6a40c22cfd9495e4614888bbf232e1b617b6
Salah Nahed <17902030+salahamassi@users.noreply.github.com>
2023-06-24 00:03:41 +0300

    use melos

--- lib/src/form/lib/src/l10n/validator_messages_all.dart
+++
@@ -1,7 +0,0 @@
-// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
-// This is a library that looks up messages for specific locales by
-// delegating to the appropriate library.
-
-export 'validator_messages_all_locales.dart'
-  show initializeMessages;
-
