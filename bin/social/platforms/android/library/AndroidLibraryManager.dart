a04ddc9c90396e2cc6480a65058d8d8c017a3f70
Kareem Radwan <kareem.e.radwan@gmail.com>
2023-06-23 13:50:03 +0300

    implement AndroidLibraryManager

--- bin/social/platforms/android/library/AndroidLibraryManager.dart
+++ bin/social/platforms/android/library/AndroidLibraryManager.dart
@@ -1,41 +1,171 @@
 import 'dart:io';
 
+import '../../../../util/console.dart';
+import '../../../../util/file.dart';
 import 'AndroidLibrary.dart';
 import 'AndroidLibraryInterface.dart';
 
 class AndroidLibraryManager implements AndroidLibraryInterface {
-
   File buildFile;
+  final Map<int, AndroidLibrary> _cache = {};
 
   AndroidLibraryManager(this.buildFile);
 
   @override
   Future<List<AndroidLibrary>> listLibraries() async {
-    throw Exception("not implemented yet");
+    _cache.clear();
+    var result = await buildFile.search("implementation");
+
+    if (result.isEmpty) {
+      return [];
+    }
+    List<AndroidLibrary> libs = [];
+    for (var element in result) {
+      var rawLibrary = element.line
+          .trim()
+          .replaceAll("implementation ", "")
+          .replaceAll("'", "")
+          .replaceAll('"', '');
+
+      if (rawLibrary != '') {
+        var parts = rawLibrary.split(":");
+        var libraryVersion = parts.removeLast();
+        var libraryName = parts.join(":");
+
+        if (libraryVersion.startsWith("\$")) {
+          var resolveVersionResult =
+              await File("${Directory.current.path}/android/build.gradle")
+                  .search("ext.${libraryVersion.replaceAll("\$", "")}");
+          if (resolveVersionResult.isNotEmpty) {
+            var version = resolveVersionResult[0]
+                .line
+                .replaceAll("ext.${libraryVersion.replaceAll("\$", "")} = ", "")
+                .replaceAll("'", "")
+                .replaceAll('"', '');
+            libraryVersion = version;
+          }
+        }
+
+        var library = AndroidLibrary(libraryName, libraryVersion.trim());
+        libs.add(library);
+        _cache[element.index] = library;
+      }
+    }
+
+    return libs;
   }
 
   @override
   Future<AndroidLibrary> getLibrary(String name, String version) async {
-    throw Exception("not implemented yet");
+    await listLibraries();
+    var items = _cache.values
+        .where((element) => element.name == name && element.version == version);
+
+    if (items.isEmpty) {
+      throw Exception(
+          "No Android Library found with name: ${name} and version: ${version}");
+    }
+    return items.first;
   }
 
   @override
   Future<List<AndroidLibrary>> getLibraries(String name) async {
-    throw Exception("not implemented yet");
+    await listLibraries();
+
+    var items = _cache.values.where((element) => element.name == name).toList();
+    if (items.isEmpty) {
+      throw Exception("No Android Library found with name: ${name}");
+    }
+    return items;
   }
 
   @override
   Future<void> updateLibrary(AndroidLibrary library) async {
-    throw Exception("not implemented yet");
+    List<SearchResult> result = [];
+    result = await buildFile.search("dependencies");
+    if (result.isEmpty) {
+      throw Exception("No libraries tag into /android/app/build.gradle");
+    }
+    await listLibraries();
+
+    var libraryIndex = -1;
+    for (var element in _cache.entries) {
+      if (element.value.name == library.name &&
+          element.value.version.trim() == library.version.trim()) {
+        libraryIndex = element.key;
+        break;
+      }
+    }
+
+    if (libraryIndex == -1) {
+      printWarning("No Android Library Matched ");
+      return;
+    }
+
+    Map<int, String> linesIndex = await buildFile.linesIndexed();
+
+    String content = "";
+    linesIndex.forEach((key, value) {
+      if (key != libraryIndex) {
+        content += '    implementation "${library.name}:${library.version}"\n';
+      }
+    });
+
+    buildFile.writeAsStringSync(content);
   }
 
   @override
   Future<void> removeLibrary(AndroidLibrary library) async {
-    throw Exception("not implemented yet");
+    List<SearchResult> result = [];
+    result = await buildFile.search("dependencies");
+    if (result.isEmpty) {
+      throw Exception("No dependencies tag into /android/build.gradle");
+    }
+    await listLibraries();
+    var libraryIndex = -1;
+    for (var element in _cache.entries) {
+      if (element.value.name == library.name &&
+          element.value.version.trim() == library.version.trim()) {
+        libraryIndex = element.key;
+        break;
+      }
+    }
+
+    if (libraryIndex == -1) {
+      printWarning("No Android Library Matched ");
+      return;
+    }
+
+    Map<int, String> linesIndex = await buildFile.linesIndexed();
+
+    String content = "";
+    linesIndex.forEach((key, value) {
+      if (key != libraryIndex) {
+        content += "$value\n";
+      }
+    });
+
+    buildFile.writeAsStringSync(content);
   }
 
   @override
   Future<void> addLibrary(AndroidLibrary library) async {
-    throw Exception("not implemented yet");
+    List<SearchResult> result = [];
+    result = await buildFile.search("dependencies");
+    if (result.isEmpty) {
+      throw Exception("No dependencies tag into /android/app/build.gradle");
+    }
+
+    Map<int, String> linesIndex = await buildFile.linesIndexed();
+
+    var content = "";
+    linesIndex.forEach((key, value) {
+      if (key == result[0].index + 1) {
+        content += '    implementation "${library.name}:${library.version}"\n';
+      }
+      content += "$value\n";
+    });
+
+    buildFile.writeAsStringSync(content);
   }
 }
--- bin/social/providers/google/GoogleDriver.dart
+++ bin/social/providers/google/GoogleDriver.dart
@@ -1,23 +1,36 @@
+import '../../../util/console.dart';
 import '../../SocialDriver.dart';
 import '../../platforms/android/AndroidManager.dart';
+import '../../platforms/android/library/AndroidLibrary.dart';
 import '../../platforms/android/plugin/AndroidPlugin.dart';
 
 class GoogleDriver implements SocialDriver {
   @override
   Future<void> handleAndroid(AndroidManager manager) async {
+
+    await manager.addLibrary(AndroidLibrary("com.kradwan:dsadsa", "1.23"));
+
+
     var plugins = await manager.listPlugins();
     for (var element in plugins) {
-      print(
+      printWarning(
           "Plugin: ${element.name.padRight(32, ' ')}  version: ${element.version}");
     }
 
+    print("\n\n");
+
+    var libs = await manager.listLibraries();
+    for (var element in libs) {
+      printWarning(
+          "Library: ${element.name.padRight(32, ' ')}  version: ${element.version}");
+    }
     // await manager
     //     .addPlugin(AndroidPlugin("com.google.gms:google-services", "4.3.15"));
     // await manager.addPlugin(AndroidPlugin(
     //     "com.google.firebase:firebase-crashlytics-gradle", "2.8.1"));
 
-    await manager.removePlugin(
-        AndroidPlugin("org.jetbrains.kotlin:kotlin-gradle-plugin", "1.6.10"));
+    // await manager.removePlugin(AndroidPlugin("org.jetbrains.kotlin:kotlin-gradle-plugin", "1.6.10"));
+
   }
 
   @override
