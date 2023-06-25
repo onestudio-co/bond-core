416b7f42d5a812cb4f29f408a089da38378b059c
Obeida Raida <70585602+obeidareda37@users.noreply.github.com>
2023-06-25 17:56:53 +0300

    import necessary rule in bond form

---
+++ packages/form/lib/src/validation/rules/rule.dart
@@ -0,0 +1,26 @@
+library Validation_Rule;
+
+export 'alpha.dart';
+export 'alpha_dash.dart';
+export 'alpha_num.dart';
+export 'between.dart';
+export 'boolean.dart';
+export 'date.dart';
+export 'different.dart';
+export 'digits.dart';
+export 'digits_between.dart';
+export 'email.dart';
+export 'in_list.dart';
+export 'integer.dart';
+export 'max_lengh.dart';
+export 'max_value.dart';
+export 'min_length.dart';
+export 'min_value.dart';
+export 'not_in_list.dart';
+export 'numeric.dart';
+export 'regex.dart';
+export 'required.dart';
+export 'required_if.dart';
+export 'same.dart';
+export 'size.dart';
+export 'url.dart';
