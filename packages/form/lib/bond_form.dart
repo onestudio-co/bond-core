796990a335f581167c3547aba29e7706220e752a
Obeida Raida <70585602+obeidareda37@users.noreply.github.com>
2023-06-25 17:56:53 +0300

    import necessary rule in bond form

--- packages/form/lib/bond_form.dart
+++ packages/form/lib/bond_form.dart
@@ -2,4 +2,5 @@ library form;
 
 export 'src/form_fields/form_field_state.dart';
 export 'src/validation/validation_rule.dart';
+export 'src/validation/rules/rule.dart';
 export 'src/l10n/validator_localizations.dart';
