d7f0f7a648c3f9b6f625b484e6b6972cfbe51ae5
Obeida Raida <70585602+obeidareda37@users.noreply.github.com>
2023-06-26 02:58:48 +0300

    call import form state in bond form package

--- packages/form/lib/bond_form.dart
+++ packages/form/lib/bond_form.dart
@@ -1,6 +1,7 @@
 library form;
 
 export 'src/form_fields/form_field_state.dart';
+export 'src/form_state.dart';
 export 'src/validation/validation_rule.dart';
 export 'src/validation/rules/rule.dart';
 export 'src/l10n/validator_localizations.dart';
