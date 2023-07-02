d94669ee8c39aa489890ae41aeb7e03649ad767b
Mahmoud Hafez Darwish <43539445+MahmoudHafezDarwish@users.noreply.github.com>
2023-07-02 14:03:33 +0300

    refactor chat message model

---
+++ packages/chat_bot/lib/src/data/models/message_rule.dart
@@ -0,0 +1,13 @@
+part of 'chat_message.dart';
+
+class MessageRule {
+  int? defaultValue;
+  int? min;
+  int? max;
+
+  MessageRule({
+    this.defaultValue,
+    this.min,
+    this.max,
+  });
+}
