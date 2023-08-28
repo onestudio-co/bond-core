3e303b238fcf421ca040c3c79f667ccffdf5dfb0
Salah Nahed <17902030+salahamassi@users.noreply.github.com>
2023-08-28 23:35:12 +0300

    update example project

---
+++ packages/form/packages/bond_form_riverpod/example/lib/app/routes.dart
@@ -0,0 +1,11 @@
+import 'package:example/features/auth/login_page.dart';
+import 'package:go_router/go_router.dart';
+
+final routes = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const LoginPage(),
+    ),
+  ],
+);
--- packages/form/packages/bond_form_riverpod/example/lib/main.dart
+++ packages/form/packages/bond_form_riverpod/example/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:example/routes/app_router.dart';
 import 'package:bond_core/bond_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -6,10 +5,8 @@ import 'app/app_run_tasks.dart';
 import 'bond_app.dart';
 
 void main() => run(
-      () => ProviderScope(
-        child: BondApp(
-          appRouter: sl<AppRouter>(),
-        ),
+      () => const ProviderScope(
+        child: BondApp(),
       ),
       RunAppTasks(),
     );
