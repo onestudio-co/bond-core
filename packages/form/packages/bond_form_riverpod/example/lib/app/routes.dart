9f5cef44c36e050a2a81e68e3accd7037bfb40a9
Obeida Raida <70585602+obeidareda37@users.noreply.github.com>
2023-09-10 08:46:23 +0300

    register page in routes and redirect from login page

--- packages/form/packages/bond_form_riverpod/example/lib/app/routes.dart
+++ packages/form/packages/bond_form_riverpod/example/lib/app/routes.dart
@@ -1,4 +1,5 @@
 import 'package:example/features/auth/presentations/login_page.dart';
+import 'package:example/pizza/presentations/pizza_order_page.dart';
 import 'package:go_router/go_router.dart';
 
 final routes = GoRouter(
@@ -6,6 +7,10 @@ final routes = GoRouter(
     GoRoute(
       path: '/',
       builder: (context, state) => const LoginPage(),
+    ),
+    GoRoute(
+      path: '/pizza',
+      builder: (context, state) => const PizzaOrderPage(),
     ),
   ],
 );
