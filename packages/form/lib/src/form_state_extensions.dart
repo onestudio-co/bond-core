ebe676a4a61d9b0b947bf6708a508b230d004f0b
Salah Nahed <17902030+salahamassi@users.noreply.github.com>
2023-09-15 00:28:46 +0300

    Update form_state_extensions.dart

--- packages/form/lib/src/form_state_extensions.dart
+++ packages/form/lib/src/form_state_extensions.dart
@@ -1,65 +1,35 @@
 import 'package:bond_form/bond_form.dart';
 
-/// Extension methods for [BondFormState].
-///
-/// These methods are designed to provide convenience operations on
-/// [BondFormState] instances for managing and accessing form fields.
+/// Extension methods for [BondFormState] providing convenience operations.
 extension XBondFormState on BondFormState {
-  /// Gets a list of [RadioButtonFieldState] for a specified field name.
+  /// Retrieves a list of [RadioButtonFieldState] for a specified [fieldName].
   ///
-  /// This method fetches the list of radio button states for the given
-  /// [fieldName] by first retrieving the corresponding [RadioGroupFieldState].
+  /// This method simplifies fetching radio button states for the given [fieldName].
   ///
-  /// Example usage:
-  /// ```dart
-  /// final radioButtons = bondFormState.radioButtonsOf<String>('fieldName');
-  /// ```
-  ///
-  /// - [fieldName]: The name of the field whose radio buttons we want to get.
-  /// - Returns: A list of [RadioButtonFieldState] that belong to the field
-  ///   specified by [fieldName].
-  ///
-  /// Throws:
-  /// - [ArgumentError] if no field with [fieldName] exists.
+  /// - Parameter [fieldName]: The name of the field to fetch.
+  /// - Returns: A list of [RadioButtonFieldState] instances.
+  /// - Throws: [ArgumentError] if the field doesn't exist.
   List<RadioButtonFieldState<T>> radioButtonsOf<T>(String fieldName) {
     return get<RadioGroupFieldState<T>, T?>(fieldName).radioButtons;
   }
 
-  /// Retrieves the current group value of a `RadioGroupFieldState` associated with the given [fieldName].
+  /// Retrieves the value of a `RadioGroupFieldState` for a specified [fieldName].
   ///
-  /// This method simplifies the process of getting the current value for a radio button group in a form.
-  /// The generic type `T` refers to the type of the value that the radio button group holds.
+  /// This method simplifies fetching the current value of a radio button group.
   ///
-  /// Example usage:
-  /// ```dart
-  /// final sizeValue = formState.radioGroupValue<String>('size');
-  /// ```
-  ///
-  /// - [fieldName]: The name of the field corresponding to the radio button group.
-  ///
-  /// Returns:
-  /// The current value of the radio button group, or `null` if not set.
+  /// - Parameter [fieldName]: The name of the field to fetch.
+  /// - Returns: The current value of the field or `null` if not set.
   T? radioGroupValue<T>(String fieldName) {
     return get<RadioGroupFieldState<T>, T?>(fieldName).value;
   }
 
-  /// Fetches the list of [CheckboxFieldState] objects associated with a given field name.
+  /// Retrieves a list of [CheckboxFieldState] for a specified [fieldName].
   ///
-  /// This utility method simplifies the process of retrieving the checkbox states
-  /// for a particular field within the form. The method is generic and works for checkboxes
-  /// of any type `T`.
+  /// This method simplifies fetching checkbox states for the given [fieldName].
   ///
-  /// [fieldName] is the name of the field whose checkbox states you want to retrieve.
-  ///
-  /// Returns a list of [CheckboxFieldState<T>] objects representing the state of each checkbox
-  /// within the specified field.
-  ///
-  /// Example:
-  /// ```dart
-  /// final toppings = formState.checkboxesOf<PizzaTopping>('toppings');
-  /// ```
-  ///   /// Throws:
-  /// - [ArgumentError] if no field with [fieldName] exists.
+  /// - Parameter [fieldName]: The name of the field to fetch.
+  /// - Returns: A list of [CheckboxFieldState] instances.
+  /// - Throws: [ArgumentError] if the field doesn't exist.
   List<CheckboxFieldState<T>> checkboxesOf<T>(String fieldName) {
     if (T == dynamic) {
       throw ArgumentError(
@@ -68,55 +38,33 @@ extension XBondFormState on BondFormState {
     return get<CheckboxGroupFieldState<T>, Set<T>?>(fieldName).checkboxes;
   }
 
-  /// Fetches the set of values for checkboxes associated with a given field name.
+  /// Retrieves the selected values of a checkbox group for a specified [fieldName].
   ///
-  /// This utility method simplifies the process of retrieving the selected values
-  /// for a checkbox group field within the form. The method is generic and works for
-  /// checkboxes of any type `T`.
+  /// This method simplifies fetching selected checkbox values.
   ///
-  /// [fieldName] is the name of the field whose checkbox values you want to retrieve.
-  ///
-  /// Returns a set of selected values for the checkboxes of type `T`.
-  ///
-  /// Example:
-  /// ```dart
-  /// final selectedToppings = formState.checkboxValues<PizzaTopping>('toppings');
-  /// ```
+  /// - Parameter [fieldName]: The name of the field to fetch.
+  /// - Returns: A set containing the selected values.
   Set<T> checkboxValues<T>(String fieldName) {
     return get<CheckboxGroupFieldState<T>, Set<T>>(fieldName).value ?? {};
   }
 
-  /// Fetches the first selected value for checkboxes associated with a given field name.
+  /// Retrieves the first selected value of a checkbox group for a specified [fieldName].
   ///
-  /// This utility method retrieves the first selected checkbox value for a particular
-  /// field within the form. If no checkbox is selected, it returns null.
+  /// This method simplifies fetching the first selected checkbox value.
   ///
-  /// [fieldName] is the name of the field whose first checkbox value you want to retrieve.
-  ///
-  /// Returns the first selected value of type `T` or null if no checkboxes are selected.
-  ///
-  /// Example:
-  /// ```dart
-  /// final firstTopping = formState.checkboxValue<PizzaTopping>('toppings');
-  /// ```
+  /// - Parameter [fieldName]: The name of the field to fetch.
+  /// - Returns: The first selected value or `null` if none are selected.
   T? checkboxValue<T>(String fieldName) {
     return checkboxValues<T>(fieldName).firstOrNull;
   }
 
-  /// Checks if a specific checkbox value is selected in the checkbox group.
+  /// Checks if a specific value is selected within a checkbox group.
   ///
-  /// Takes the `fieldName` of the checkbox group and the `value` of the specific checkbox
-  /// as parameters. Returns `true` if the checkbox with the given `value` is selected,
-  /// otherwise returns `false`.
+  /// This method checks if a specific checkbox value is selected.
   ///
-  /// - [fieldName]: The name of the checkbox group field.
-  /// - [value]: The value of the specific checkbox to be checked.
-  ///
-  /// ```
-  /// final isSelected = checkboxSelected('toppings', Toppings.mushrooms);
-  /// ```
-  ///
-  /// This will return `true` if the "Mushrooms" checkbox is selected in the "toppings" group.
+  /// - Parameter [fieldName]: The name of the checkbox group.
+  /// - Parameter [value]: The specific value to check for.
+  /// - Returns: `true` if the checkbox with the given value is selected, otherwise `false`.
   bool checkboxSelected<T>(String fieldName, T value) {
     return checkboxValues<T>(fieldName).contains(value);
   }
