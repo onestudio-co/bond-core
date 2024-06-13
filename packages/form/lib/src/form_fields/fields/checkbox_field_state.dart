b424ae8ee56a96891a18737d5ce734af3c5774f4
Salah Nahed <salahamassi@users.noreply.github.com>
2024-06-13 03:21:09 +0300

    refactor(form): Update generic type for CheckboxFieldState and RadioGroupFieldState

--- packages/form/lib/src/form_fields/fields/checkbox_field_state.dart
+++ packages/form/lib/src/form_fields/fields/checkbox_field_state.dart
@@ -1,21 +1,21 @@
-import 'package:bond_form/src/validation/validation_rule.dart';
 import 'package:bond_form/src/form_fields/form_field_state.dart';
+import 'package:bond_form/src/validation/validation_rule.dart';
 
 /// Represents the state of a checkbox form field.
 ///
 /// A checkbox form field extends the [FormFieldState] class, adding support for
 /// handling the state of a checkbox input.
-class CheckboxFieldState<T> extends FormFieldState<T?> {
+class CheckboxFieldState<T> extends FormFieldState<T> {
   /// Creates a new instance of [CheckboxFieldState].
   ///
   /// - [value]: The initial value of the checkbox (checked or unchecked).
   /// - [label]: The label or name of the checkbox form field (required).
   /// - [rules]: The list of validation rules to apply to the checkbox field (default is an empty list).
   CheckboxFieldState(
-    T? value, {
+    T value, {
     required String label,
     String? error,
-    List<ValidationRule<T?>> rules = const [],
+    List<ValidationRule<T>> rules = const [],
   }) : super(
           value: value,
           label: label,
@@ -30,7 +30,7 @@ class CheckboxFieldState<T> extends FormFieldState<T?> {
     String? label,
     bool? touched,
     bool? validateOnUpdate,
-    List<ValidationRule<T?>>? rules,
+    List<ValidationRule<T>>? rules,
   }) {
     return CheckboxFieldState<T>(
       value ?? this.value,
