49a1f491aef0f0e385152febac543f4bc3f454db
Salah Nahed <salahamassi@users.noreply.github.com>
2024-06-19 03:55:10 +0300

    feat(cache): Add tests for observer not removed when key is deleted and all keys are deleted

--- packages/cache/test/cache/cache_observable_test.dart
+++ packages/cache/test/cache/cache_observable_test.dart
@@ -12,30 +12,60 @@ void main() {
     });
 
     test('Observers are added correctly', () {
-      var key = 'test_key';
-      var observer = MockObserver();
+      final key = 'test_key';
 
+      // Create an observer and watch the key
+      final observer = MockObserver();
       cache.watch(key, observer);
 
+      // Verify that the observer is watching the key
       expect(cache.observers[key]?.containsObserver(observer), isTrue);
     });
 
     test('Observer limit is enforced', () {
-      var key = 'test_key';
+      final key = 'test_key';
+
+      // Add more observers than the limit
       for (var i = 0; i < cache.maxObserversPerKey + 2; i++) {
         var observer = MockObserver();
         cache.watch(key, observer);
       }
+
+      // Verify that the number of observers is equal to the limit
       expect(cache.observers[key]?.length, cache.maxObserversPerKey);
     });
 
     test('Observers are removed correctly', () {
-      var key = 'test_key';
-      var observer = MockObserver();
+      final key = 'test_key';
 
+      // Create an observer and watch the key
+      final observer = MockObserver();
       cache.watch(key, observer);
+
+      // unwatch the observer
       cache.unwatch(key, observer);
+
+      // Verify that the observer is no longer watching the key
       expect(cache.observers[key]?.contains(observer), isFalse);
+    });
+
+    test('Observers are removed when called unwatch with null observer', () {
+      final key = 'test_key';
+
+      // Create two observers and watch the key
+      final observer1 = MockObserver();
+      final observer2 = MockObserver();
+      cache.watch(key, observer1);
+      cache.watch(key, observer2);
+
+      // Verify that both observers are watching the key
+      expect(cache.observers[key]?.length, equals(2));
+
+      // Remove all observers for the key
+      cache.unwatch(key);
+
+      // Verify that the observers are no longer watching the key
+      expect(cache.observers[key], isNull);
     });
   });
 
@@ -47,11 +77,14 @@ void main() {
     });
 
     test('Observer calls onUpdate when notified of a change', () {
-      var key = 'test_key';
-      var newValue = 'newValue';
-      final observer = MockObserver<String>();
+      final key = 'test_key';
+      final newValue = 'newValue';
 
+      // Create an observer and watch the key
+      final observer = MockObserver<String>();
       cache.watch<String>(key, observer);
+
+      // Notify the observer that the key has been updated
       cache.notifyObservers(key, newValue);
 
       // Verify that onUpdate was called with the correct key and value
@@ -68,6 +101,40 @@ void main() {
 
       // Verify that onDelete was called with the correct key
       verify(observer.onDelete(key)).called(1);
+    });
+
+    test('Observer does not removed when a key is deleted', () {
+      final key = 'test_key';
+
+      // Create an observer and watch the key
+      final observer = MockObserver<int>();
+      cache.watch<int>(key, observer);
+
+      // Notify the observer that the key has been deleted
+      cache.notifyDeleteObservers(key);
+
+      // Notify the observer that the key has been updated
+      cache.notifyObservers(key, 42);
+
+      // Verify that the observer is still watching the key
+      expect(cache.observers[key]?.containsObserver(observer), isTrue);
+      verify(observer.onDelete(key)).called(1);
+    });
+
+    test('Observer does not removed when all keys are deleted', () {
+      final key = 'test_key';
+
+      // Create an observer and watch the key
+      final observer = MockObserver<int>();
+      cache.watch<int>(key, observer);
+
+      // Notify the all observers that all keys have been deleted
+      cache.notifyDeleteObservers(CacheObservable.allKeys);
+      cache.notifyObservers(key, 42);
+
+      // Verify that the observer is still watching the key
+      expect(cache.observers[key]?.containsObserver(observer), isTrue);
+      verify(observer.onUpdate(key, 42)).called(1);
     });
   });
 }
