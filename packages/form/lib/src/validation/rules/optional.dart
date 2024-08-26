05a31c9e49a213eda01de3b7ae3f095fb3026b9e
Salah Nahed <salahamassi@users.noreply.github.com>
2024-08-26 03:26:10 +0300

    feat(form): Add optional validation rule

---
+++ packages/form/lib/src/validation/rules/optional.dart
@@ -0,0 +1,26 @@
+import 'package:bond_form/src/form_fields/form_field_state.dart';
+import 'package:bond_form/src/validation/validation_rule.dart';
+
+/// A validation rule that marks a field as optional.
+///
+/// If the value is not present (null, empty string, or empty list),
+/// the validation passes and skips all other rules.
+/// If the value is present, other rules will be validated against the value.
+class Optional<T> extends ValidationRule<T> {
+  /// Creates a new instance of the [Optional] validation rule.
+  Optional({String? message}) : super(message);
+
+  @override
+  String validatorMessage(String fieldName) => '';
+
+  @override
+  bool validate(T value, Map<String, FormFieldState> fields) {
+    // If the value is null, an empty string, or an empty list, skip validation
+    if (value == null || (value is String && value.isEmpty) || (value is List && value.isEmpty)) {
+      return true;
+    }
+
+    // If the value is present, we proceed to validate with other rules
+    return false;  // Returning false indicates that other rules should now validate this field
+  }
+}
